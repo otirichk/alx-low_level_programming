8b18127b68186bb86da7d05859f8b1082db7f0d9
otirichk <otienorichard722@gmail.com>
2022-07-03 00:16:03 -0700

    Cleaner code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 	int Num1;
 	int Num2;
 
-	for (Num1 = 0; Num1 < 10; Num1++)
+	for (Num1 = 0; Num1 < 9; Num1++)
 	{
-		for (Num2 = 0; Num2 < 10; Num2++)
+		for (Num2 = Num1 + 1; Num2 < 10; Num2++)
 		{
 			putchar((Num1 % 10) + '0');
 			putchar((Num2 % 10) + '0');
 
-			if (Num1 == 9 && Num2 == 9)
+			if (Num1 == 8 && Num2 == 9)
 			{
 				continue;
 			}
