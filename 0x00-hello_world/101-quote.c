9383a54f6411d3a3765034f9cecdb4e8bf3f2323
otirichk <otienorichard722@gmail.com>
2022-07-02 23:19:56 -0700

    Final code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - writes statement to standard error
+ *
+ * Return: always 1
+ *
+ */
+
+int main(void)
+{
+	writes("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+
+	return (1);
+}
