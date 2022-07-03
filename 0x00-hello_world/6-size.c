d0aff0aeb36b922e88cf4b5d99b8e21e781774ac
otirichk <otienorichard722@gmail.com>
2022-07-02 23:07:45 -0700

    Cleaner code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-/*
- * main - entry point 
- *
+/**
+ * main - entry point
  * program prints various types on the machine it is compiled and run on
+ *
+ * Return: always 0
+ *
  */
+
 int main(void)
 {
-	printf("the size of a character is %d bytes.\n", sizeof(char));
-	printf("The size of an integer is %d bytes.\n", sizeof(int));
-	printf("The size of a long integer is %d bytes.\n", sizeof(long int));
-	printf("The size of long long integer is %d bytes\n", sizeof(long long int));
-	printf("The size of a float is %d bytes\n", sizeof(float));
+	printf("the size of a character is %zu bytes.\n", sizeof(char));
+	printf("The size of an integer is %zu bytes.\n", sizeof(int));
+	printf("The size of a long integer is %zu bytes.\n", sizeof(long int));
+	printf("The size of long long integer is %zu bytes\n", sizeof(long long int));
+	printf("The size of a float is %zu bytes\n", sizeof(float));
 	return (0);
 }
