8714b75d1b2489e3822cabffe7c02608c846dfe3
otirichk <otienorichard722@gmail.com>
2022-07-02 22:57:52 -0700

    Clean code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - entry point
  * program uses "printf" function to print to stdout
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
