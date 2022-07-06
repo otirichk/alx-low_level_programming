975f4df26926816b650fb0f38549fa0ee36267a6
otirichk <otienorichard722@gmail.com>
2022-07-06 09:13:07 -0700

    Print the multiplication table of 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 		for (Mult2 = 0; Mult2 <= 9; Mult2++)
 		{
 			Comp = (Mult1 * Mult2);
-			if (Comp > 10)
+			if (Comp >= 10)
 			{
 				_putchar ((Comp / 10) + '0');
 				_putchar((Comp % 10) + '0');
@@ -25,7 +25,6 @@ void times_table(void)
 				_putchar(Comp + '0');
 			}
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 		}
 		_putchar('\n');
