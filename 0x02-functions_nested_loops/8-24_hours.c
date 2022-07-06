a1904bd2a7d4b325173547b6dbb571b1c2810891
otirichk <otienorichard722@gmail.com>
2022-07-06 08:17:38 -0700

    Print minutes of the 24 hour clock

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,14 +16,13 @@ void jack_bauer(void)
 	{
 		for (H2 = 0; H2 <= 9; H2++)
 		{
-			_putchar(H1 + '0');
-			_putchar(H2 + '0');
-			_putchar(':');
-
 			for (M1 = 0; M1 <= 5; M1++)
 			{
 				for (M2 = 0; M2 <= 9; M2++)
 				{
+					_putchar(H1 + '0');
+					_putchar(H2 + '0');
+					_putchar(':');
 					_putchar(M1 + '0');
 					_putchar(M2 + '0');
 				}
