1ffbd349e76d8ba4f96858200673f39bff6b7afe
otirichk <otienorichard722@gmail.com>
2022-07-06 22:51:17 -0700

    print to 9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n < 98)
+		while (n <= 98)
 		{
 			printf("%d, ", n);
 			n++;
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
 			printf("%d, ", n);
 			n--;
