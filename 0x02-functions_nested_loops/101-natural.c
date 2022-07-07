190a1aa16e55a9ee95ddc918b953cfb0a35be879
otirichk <otienorichard722@gmail.com>
2022-07-07 01:01:30 -0700

    print sum of multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,21 +5,21 @@
  * main - prints the sum of all multiples of 5 or 5
  * less than 1024
  *
- * Return: sum 
+ * Return: sum
  *
  */
 
-int main (void)
+int main(void)
 {
 	int m, sum;
-	
+
 	for (m = 0; m < 1024; m++)
 	{
 		if (m % 3 == 0 || m % 5 == 0)
 		{
-			sum +=m;
+			sum += m;
 		}
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 	return (0);
 }
