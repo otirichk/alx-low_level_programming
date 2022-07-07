d623119f2eecb2acc05344df7f1490427d7bdaf0
otirichk <otienorichard722@gmail.com>
2022-07-07 01:27:19 -0700

    Fibonacci Series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,12 +13,26 @@
 
 int main(void)
 {
-	int i, series;
+	int i;
+	unsigned int Num1 = 0;
+	unsigned int Num2 = 1;
+	unsigned int Sum;
 
 	for (i = 1; i < 50; i++)
 	{
-		series += i;
-		printf("%d, ", series);
+		Sum = Num1 + Num2;
+		printf("%lu, ", Sum);
+		Num1 = Num 2;
+		Num2 = Sum;
+
+		if (i == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
 	}
 	return (0);
 }
