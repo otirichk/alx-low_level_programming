4416e629d43481d2ff12475a44bd136bf1a5d402
otirichk <otienorichard722@gmail.com>
2022-07-07 13:24:12 -0700

    Nested loops and functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1when uppercase and 0 otherwise
+ *
+ * @c: character being checked
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c > 'A' && c < 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This README file is for more nested loops directory 
+
+0-isupper.c => Returns 1 when letter is uppercase and 0 otherwise
