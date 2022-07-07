7ccd315edad80c71fb27e9ed6686f72024a0aa99
otirichk <otienorichard722@gmail.com>
2022-07-06 23:58:56 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,8 @@ void print_times_table(int n)
 				else
 				{
 					_putchar((Comb / 100) + '0');
-					_putchar((Comb / 10) % 10 + '0');
+					_putchar((Comb / 10) + '0');
+					_putchar((Comb % 10) + '0');
 				}
 			}
 			_putchar('\n');
