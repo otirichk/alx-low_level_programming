995f632e72cdc8081f124df3d89be9aa626ad621
otirichk <otienorichard722@gmail.com>
2022-07-07 01:47:42 -0700

    Print event values of Fibonacci series

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints fibonacci series of numbers
+ * less than 50
+ * starting with 1 and 2
+ *
+ * Return: series
+ *
+ */
+
+
+int main(void)
+{
+	int i;
+	unsigned long Num1 = 0;
+	unsigned long Num2 = 1;
+	unsigned long Sum;
+
+	for (i = 1; i <= 50; i++)
+	{
+		Sum = Num1 + Num2;
+		Num1 = Num2;
+		Num2 = Sum;
+		if (sum < 4000000)
+		{
+			while (sum < 4000000)
+			{
+				if (n % 2 == 0)
+				{
+					printf("%lu" n);
+				}
+			}
+		}
+		if (sum > 4000000)
+		{
+			break;
+		}
+	}
+	return (0);
+}
