9b070d1cd355e9b3b5e49cb28004cc54dfaf9edd
otirichk <otienorichard722@gmail.com>
2022-07-12 22:58:11 -0700

    Fixed code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,10 +14,10 @@ void rev_string(char *s)
 
 	char tmp;
 
-	for (index = len - 1; index >= len / 2; index--)
+	for (index = len2 - 1; index >= len2 / 2; index--)
 	{
 		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
+		s[index] = s[len2 - index - 1];
+		s[len2 - index - 1] = tmp;
 	}
 }
