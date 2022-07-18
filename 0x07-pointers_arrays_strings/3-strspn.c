7d71f25bbfa0eac106030230537968bb8cebdbcf
otirichk <otienorichard722@gmail.com>
2022-07-18 13:15:20 -0700

    Fixes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - ets the length of a prefix substring.
+ *
+ * @s: initial string
+ * @accept: accepted
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m, n = 0, p = 0;
+
+	while (accept[n])
+	{
+		m = 0;
+		while (s[m] != 32)
+		{
+			if (accept[n] == s[m])
+			{
+				p++;
+			}
+			m++;
+		}
+		n++
+	}
+	return (p);
+}
