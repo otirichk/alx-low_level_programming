daa10037a21411d943d34f8749a2147e3cb43449
otirichk <otienorichard722@gmail.com>
2022-07-19 02:33:17 -0700

    Check for string set

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - locates the first occurence of a substring
+ *
+ * @haystack: the main string
+ * @needle: the reference string to be located
+ *
+ * Return: pointer to the located substring
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int num1 = 0, num2 = 0;
+
+	while (haystack[num1])
+	{
+		while (needle[num2])
+		{
+			if (haystack[num1 + num2] != needle[num2])
+			{
+				break;
+
+			}
+			num2++;
+		}
+		if (needle[num2] == '\0')
+		{
+			return (haystack + a);
+		}
+		num1++;
+	}
+	return ('\0');
+}
