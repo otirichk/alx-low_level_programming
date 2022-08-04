a6cae3b2adf9914fc23efec059a2971d7cb050da
otirichk <otienorichard722@gmail.com>
2022-08-04 03:37:23 -0700

    Free the grid in 3

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the grid created in the previous project
+ * 3-alloc_grid
+ *
+ * @grid: the existing grid created
+ * @height: the array of rows
+ */
+
+void free_grid(char **grid, int height)
+{
+	int count;
+
+	for (count = 0; count < height; count++)
+	{
+		free(grid[count]);
+	}
+	free(grid);
+}
